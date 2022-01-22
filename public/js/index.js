1a53a4d6bf99ac2ed165cee0a22d2de3cee8fbae
lmonteiro18 <luis-eduardo@live.com.pt>
2022-01-22 15:56:54 +0000

    Novo site

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,53 @@
-let titulosArtigos = document.getElementsByClassName('titulo-artigo');
-let listaSubtitulos = document.getElementsByClassName('lista-subtitulos');
+let titlesDiv = document.querySelectorAll(".artigo>div:first-child");
 
-titulosArtigos[0].addEventListener('click', function() {
-  listaSubtitulos[0].style.display = "inline-block";
-
-  setTimeout(function() {
-    listaSubtitulos[0].style.bottom = "-10%";
-    titulosArtigos[1].style.transform = "translate(10%, -200%)";
-    setTimeout(function(){
-      titulosArtigos[1].style.display = "none";
-    }, 3000);
-  }, 1);
+document.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  titlesDiv.forEach((div, i) => {
+    if (i === 0) {
+      div.style.transform = "translateY(calc(-90.5% + " + window.scrollY * 2 + "px))";
+    }else{
+      div.style.transform = "translateY(calc(-80% + " + window.scrollY * 2 + "px))";
+    }
+  });
 });
 
-titulosArtigos[1].addEventListener('click', function() {
-  listaSubtitulos[1].style.display = "inline-block";
 
-  setTimeout(function() {
-    listaSubtitulos[1].style.bottom = "-27%";
-  }, 1);
-});
+let botoesNavegacao = document.getElementsByClassName('botao-navegacao');
+let artigos = document.getElementsByClassName('artigo');
+let main = document.querySelector('main');
+let header = document.querySelector('header');
+
+
+for (let i = 0; i < botoesNavegacao.length; i++) {
+  botoesNavegacao[i].addEventListener("click", () => {
+    if (!artigos[0].classList.contains("showArtigo") && !artigos[1].classList.contains("showArtigo")) {
+      header.classList.add("smallHeader");
+      main.classList.add("showMain");
+      artigos[i].classList.add("showArtigo");
+      setTimeout(() => main.classList.add("changeOpacityMain"), 100);
+    } else if (artigos[i].classList.contains("showArtigo")) {
+      main.classList.remove("changeOpacityMain");
+      setTimeout(() => artigos[i].classList.remove("showArtigo"), 1000);
+      setTimeout(() => main.classList.remove("showMain"), 1000);
+      header.classList.remove("smallHeader");
+    } else if (!artigos[i].classList.contains("showArtigo")) {
+      let dist = window.scrollY;
+      let counter = 0;
+      while (dist > 0) {
+        if (counter % 30 === 0) {
+          window.scrollTo(0, dist);
+          dist -= 1;
+        }
+        console.log("Dist: " + dist);
+        console.log(window.scrollY);
+        counter++;
+      }
+      if (i === 0) {
+        artigos[1].classList.remove("showArtigo");
+      } else {
+        artigos[0].classList.remove("showArtigo");
+      }
+      artigos[i].classList.add("showArtigo");
+    }
+  });
+}
