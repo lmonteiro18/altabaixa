5d05aad20dd4b2558c76104f7a639a6f2c44e0f2
lmonteiro18 <luis-eduardo@live.com.pt>
2022-01-23 20:43:26 +0000

    Mais avanços

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,55 @@
+let open = false;
+
+let imagesDiv = document.querySelector(".artigo:first-child>div:nth-child(2)");
+//console.log(images);
+/*let checkOnce = [];
+let initialY = [];
+imagesDiv.forEach((img, i) => {
+  checkOnce[i] = false;
+});
+let vpHeight;*/
+//let distancesToTop_1 = ["0", "100vh", "200vh"];
+//let distancesToTop_2 = ["0", "100vh", ];
+
 let titlesDiv = document.querySelectorAll(".artigo>div:first-child");
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
+  let ratio = window.innerWidth / window.innerHeight;
+  imagesDiv.style.transform = "translate(calc(50vw - " + (window.scrollY * ratio) + "px), 0)";
+  /*imagesDiv.forEach((img, i) => {
+    let enterViewport = i * window.innerHeight + window.innerHeight / 4;
+    let leaveViewport = i * window.innerHeight + 6.5 * window.innerHeight / 3;
+    let ratio = window.innerWidth / (leaveViewport - enterViewport);
+    if (window.scrollY >= enterViewport && window.scrollY <= leaveViewport) {
+      let translateValue = ratio * (window.scrollY - enterViewport) * 1.1;
+      img.style.transform = "translateX(calc(150% - " + translateValue + "px))";
+    }*/
+
+
+  /*if (open === true && i === 0) {
+    //console.log(window.scrollY);
+    //console.log("Index: " + i + " -> " + isInViewport(img));
+    if (checkOnce[i] === false) {
+      vpHeight = getViewportHeight(img);
+      isInViewport(img);
+      checkOnce[i] = true;
+      initialY[i] = window.scrollY;
+      console.log("InitialY: " + initialY);
+      //console.log(checkOnce);
+    }
+    let translateValue = (window.scrollY - initialY[i]) * ratio;
+    img.style.transform = "translateX(calc(50%-" + translateValue * 2 + "px))";
+    if (i === 0) {
+      console.log("Translate value: " + translateValue);
+      //console.log("Transform: " + img.style.transform);
+    }
+  }*/
+  //});
   titlesDiv.forEach((div, i) => {
     if (i === 0) {
-      div.style.transform = "translateY(calc(-90.5% + " + window.scrollY * 2 + "px))";
-    }else{
-      div.style.transform = "translateY(calc(-80% + " + window.scrollY * 2 + "px))";
+      div.style.transform = "translateY(calc(-94% + " + window.scrollY * 2 + "px))";
+    } else {
+      div.style.transform = "translateY(calc(-86% + " + window.scrollY * 2 + "px))";
     }
   });
 });
@@ -20,17 +63,46 @@ let header = document.querySelector('header');
 
 for (let i = 0; i < botoesNavegacao.length; i++) {
   botoesNavegacao[i].addEventListener("click", () => {
+    let titleDivs = document.querySelectorAll(".artigo>div:first-child");
+    let textDivs = document.querySelectorAll(".artigo>div:last-child");
     if (!artigos[0].classList.contains("showArtigo") && !artigos[1].classList.contains("showArtigo")) {
+      //abrir um artigo quando ambos estão fechados
+      open = true;
       header.classList.add("smallHeader");
       main.classList.add("showMain");
       artigos[i].classList.add("showArtigo");
-      setTimeout(() => main.classList.add("changeOpacityMain"), 100);
-    } else if (artigos[i].classList.contains("showArtigo")) {
+      setTimeout(() => {
+        main.classList.add("changeOpacityMain");
+        if (i === 0) {
+          $(".artigo:first-child>div:first-child").animate({
+            top: "0"
+          }, 1000);
+        } else {
+          $(".artigo:last-child>div:first-child").animate({
+            top: "0"
+          }, 1000);
+        }
+        setTimeout(() => titleDivs[i].style.top = "0", 1000);
+        textDivs[i].style.top = "0";
+      }, 100);
+    } else if (artigos[i].classList.contains("showArtigo")) { //fechar um artigo com o seu botão
+      if (i === 0) {
+        $(".artigo:first-child>div:first-child").animate({
+          top: "-100vh"
+        }, 1000);
+      } else {
+        $(".artigo:last-child>div:first-child").animate({
+          top: "-100vh"
+        }, 1000);
+      }
+      setTimeout(() => titleDivs[i].style.top = "-100vh", 1000);
+      textDivs[i].style.top = "100vh";
       main.classList.remove("changeOpacityMain");
       setTimeout(() => artigos[i].classList.remove("showArtigo"), 1000);
       setTimeout(() => main.classList.remove("showMain"), 1000);
       header.classList.remove("smallHeader");
-    } else if (!artigos[i].classList.contains("showArtigo")) {
+    } else if (!artigos[i].classList.contains("showArtigo")) { //abrir um artigo com o seu botão e fechar o outro
+      open = true;
       let dist = window.scrollY;
       let counter = 0;
       while (dist > 0) {
@@ -43,11 +115,64 @@ for (let i = 0; i < botoesNavegacao.length; i++) {
         counter++;
       }
       if (i === 0) {
+        $(".artigo:last-child>div:first-child").animate({
+          top: "-100vh"
+        }, 1000);
+        $(".artigo:last-child>div:last-child").animate({
+          top: "100vh"
+        }, 1000);
         artigos[1].classList.remove("showArtigo");
+        setTimeout(() => {
+          titleDivs[1].style.top = "-100vh";
+          textDivs[1].style.top = "100vh";
+          $(".artigo:first-child>div").animate({
+            top: "0"
+          }, 1000);
+          artigos[0].classList.add("showArtigo");
+          setTimeout(() => {
+            titleDivs[0].style.top = "0";
+            textDivs[0].style.top = "0";
+          }, 1000);
+        }, 1000);
       } else {
+        $(".artigo:first-child>div:first-child").animate({
+          top: "-100vh"
+        }, 1000);
+        $(".artigo:first-child>div:last-child").animate({
+          top: "100vh"
+        }, 1000);
         artigos[0].classList.remove("showArtigo");
+        setTimeout(() => {
+          titleDivs[0].style.top = "-100vh";
+          textDivs[0].style.top = "100vh";
+          $(".artigo:last-child>div").animate({
+            top: "0"
+          }, 1000);
+          artigos[1].classList.add("showArtigo");
+          setTimeout(() => {
+            titleDivs[1].style.top = "0";
+            textDivs[1].style.top = "0";
+          }, 1000);
+        }, 1000);
       }
-      artigos[i].classList.add("showArtigo");
     }
   });
 }
+
+function getViewportHeight(elem) {
+  let distance = elem.getBoundingClientRect();
+  console.log(distance.top);
+
+  return distance.top;
+}
+
+function isInViewport(elem) {
+  let distance = elem.getBoundingClientRect();
+  console.log(distance.top - vpHeight);
+  return (
+    distance.top >= 0 &&
+    //distance.left >= 0 &&
+    distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) //&&
+    //distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
